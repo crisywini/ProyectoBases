b8bd2f42b8a99adfe0d6c2f2016933b99beee5bb
crisisanchezp <crisisanchezp@pop-os.localdomain>
2020-05-19 21:05:04 -0500

    INIT SUPERMARKETCAMPESTRE

---
+++ SuperMarketCampestre/src/main/java/co/uniquindio/bases/supermarket/SuperMarketCampestre/JavaMysqlejemplo.java
@@ -0,0 +1,23 @@
+package co.uniquindio.bases.supermarket.SuperMarketCampestre;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class JavaMysqlejemplo {
+
+	public static void main(String[] args) throws IOException {
+
+	}
+
+}
