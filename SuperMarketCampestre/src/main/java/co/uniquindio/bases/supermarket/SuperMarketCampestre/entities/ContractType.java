9d6c7f9e30f7c4a07bd0be99e832800354f66166
Luisa Cotte <luisacotte18@hotmail.com>
2020-05-20 00:02:33 -0500

    CREATE CONTRACT TYPE ENTITY
    No more.

---
+++ SuperMarketCampestre/src/main/java/co/uniquindio/bases/supermarket/SuperMarketCampestre/entities/ContractType.java
@@ -0,0 +1,82 @@
+package co.uniquindio.bases.supermarket.SuperMarketCampestre.entities;
+
+public class ContractType {
+
+	private int code;
+	private String name;
+	private String description;
+
+	public ContractType(int code, String name, String description) {
+
+		this.code = code;
+		this.name = name;
+		this.description = description;
+	}
+
+	public ContractType() {
+
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + code;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContractType other = (ContractType) obj;
+		if (code != other.code)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ContractType [code=" + code + ", name=" + name + ", description=" + description + "]";
+	}
+}
