571b216f945405ec7dcd7dafb9bf2955652d95c2
Luisa Cotte <luisacotte18@hotmail.com>
2020-05-20 22:21:54 -0500

    ONLY MISSING SERVICE DETAIL
    other correction all ready fine.

--- SuperMarketCampestre/src/main/java/co/uniquindio/bases/supermarket/SuperMarketCampestre/database/util/InitDatabase.java
+++ SuperMarketCampestre/src/main/java/co/uniquindio/bases/supermarket/SuperMarketCampestre/database/util/InitDatabase.java
@@ -87,8 +87,8 @@ public class InitDatabase {
 	}
 
 	public static void createAllTables() {
-		String createClientTable = "CREATE TABLE Client ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20), last_name VARCHAR(10), address VARCHAR(40), phone_number VARCHAR(10), PRIMARY KEY (code) );";
-		String createEmployeeTable = "CREATE TABLE Employee ( code VARCHAR(10) NOT NULL, name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, email VARCHAR(40) NOT NULL UNIQUE, address VARCHAR(50) NOT NULL, actual_job VARCHAR(40) NOT NULL, PRIMARY KEY (code) );"; // creoqueseborra_actualjob
+		String createClientTable = "CREATE TABLE Client ( code int NOT NULL AUTO_INCREMENT, code_service_invoice int NOT NULL, name VARCHAR(20), last_name VARCHAR(10), address VARCHAR(40), phone_number VARCHAR(10), PRIMARY KEY (code), FOREIGN KEY(code_service_invooice) REFERENCES Service_invoice (code) );";
+		String createEmployeeTable = "CREATE TABLE Employee ( code VARCHAR(10) NOT NULL, code_job int NOT NULL, name VARCHAR(20) NOT NULL, last_name VARCHAR(20) NOT NULL, email VARCHAR(40) NOT NULL UNIQUE, address VARCHAR(50) NOT NULL, PRIMARY KEY (code), FOREIGN KEY(code_job) REFERENCES Job (code) );";
 		String createPhoneNumerTable = "CREATE TABLE Phone_number (number VARCHAR(10) NOT NULL, code_employee VARCHAR(10) NOT NULL, PRIMARY KEY(number, code_employee), FOREIGN KEY(code_employee) REFERENCES Employee (code) );";
 		String createServiceTable = "CREATE TABLE Service ( code int NOT NULL AUTO_INCREMENT, details VARCHAR(40) NOT NULL, name VARCHAR(40) NOT NULL, PRIMARY KEY(code) );";
 
@@ -96,18 +96,16 @@ public class InitDatabase {
 		String createProviderTable = "CREATE TABLE Provier ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20) NOT NULL, address VARCHAR(20) NOT NULL, email VARCHAR(20), phone_number VARCHAR(10), PRIMARY KEY (code) )"; // code_auto??
 		String createProductTable = "CREATE TABLE Product ( code int NOT NULL AUTO_INCREMENT, code_provider int NOT NULL, name VARCHAR(20) NOT NULL, cantidad int NOT NULL, detalles VARCHAR(40), PRIMARY KEY(code), FOREIGN KEY(code_provider) REFERENCES Provider (code) )";
 		String createInventoryTable = "CREATE TABLE Inventory ( code int NOT NULL AUTO_INCREMENT, date VARCHAR(20) NOT NULL, PRIMARY KEY(code) )";
-		String createContractTable = "CREATE TABLE Contract ( code int NOT NULL AUTO_INCREMENT, code_employee int NOT NULL, salary DOUBLE NOT NULL, start_date VARCHAR(20) NOT NULL, end_date VARCHAR(20) NOT NULL, PRIMARY KEY (code, code_employee), FOREIGN KEY(code_employee) REFERENCES Employee (code) )"; // salario_double?
-		String createCotractTypeTable = "CREATE TABLE Contract_type ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20) NOT NULL, desciption VARCHAT(40), code_contract int NOT NULL, code_contract_employee VARCHAR(10) NOT NULL, PRIMARY KEY (code, code_contract_employee), FOREIGN KEY(code_contract) REFERENCES Contract (code), FOREIGN KEY(code_contract_employee) REFERENCES Employee (code )"; // code_contract_employee??
-		String createJobTable = "CREATE TABLE Job ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20) NOT NULL, code_employee VARCHAR(10) NOT NULL, code_contract int NOT NULL, code_contract_employee VARCHAR(10) NOT NULL, PRIMARY KEY(code, code_contract_employee), FOREIGN KEY(code_employee) REFERENCES Employee (code), FOREIGN KEY(code_contract) REFERENCES Contract (code) )"; // code_contract_employee??
-		String createSaleTable = "CREATE TABLE Sale ( code int NOT NULL AUTO_INCREMENT, code_order int NOT NULL, code_client int NOT NULL, cost double NOT NULL, order boolean NOT NULL, PRIMARY KEY(code), FOREIGN KEY(code_order) REFERENCES Order (code), FOREIGN KEY(code_client) REFERENCES Client (code) )";
-		String createOrderTable = "CREATE TABLE Order ( code int NOT NULL, phone_number VARCHAR(10) NOT NULL, address VARCHAR(20) NOT NULL, code_employee VARCHAR(10) NOT NULL, PRIMARY KEY(code), FOREIGN KEY(code_employee) REFERENCES Employee (code) )";
+		String createContractTable = "CREATE TABLE Contract ( code int NOT NULL AUTO_INCREMENT, code_employee int NOT NULL, code_contract_type int NOT NULL, salary DOUBLE NOT NULL, start_date VARCHAR(20) NOT NULL, end_date VARCHAR(20) NOT NULL, PRIMARY KEY (code, code_employee), FOREIGN KEY(code_employee) REFERENCES Employee (code), FOREIGN KEY(code_contract_type) REFERENCES Contract_type (code) )"; // salario_double?
+		String createCotractTypeTable = "CREATE TABLE Contract_type ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20) NOT NULL, desciption VARCHAT(40), code_contract int NOT NULL, PRIMARY KEY (code), FOREIGN KEY(code_contract) REFERENCES Contract (code) )";
+		String createJobTable = "CREATE TABLE Job ( code int NOT NULL AUTO_INCREMENT, name VARCHAR(20) NOT NULL, code_employee VARCHAR(10) NOT NULL, code_contract int NOT NULL, PRIMARY KEY(code), FOREIGN KEY(code_employee) REFERENCES Employee (code), FOREIGN KEY(code_contract) REFERENCES Contract (code) )";
+		String createSaleTable = "CREATE TABLE Sale ( code int NOT NULL AUTO_INCREMENT, code_order int NOT NULL, code_client int NOT NULL, code_payment_type int NOT NULL, cost double NOT NULL, order boolean NOT NULL, PRIMARY KEY(code), FOREIGN KEY(code_order) REFERENCES Order (code), FOREIGN KEY(code_client) REFERENCES Client (code) )"; // cost_double?
+		String createOrderTable = "CREATE TABLE Order ( code int NOT NULL, code_employee VARCHAR(10) NOT NULL, PRIMARY KEY(code), FOREIGN KEY(code_employee) REFERENCES Employee (code) )";
 		String createPaymentTypeTable = "CREATE TABLE Payment_type ( code int NOT NULL, code_sale int NOT NULL, name VARCHAR(20) NOT NULL, description VARCHAR(20), PRIMARY KEY(code, code_sale), FOREIGN KEY(code_sale) REFERENCES Sale (code) )";
-
-		String createEmployeeInventaryTable = "CREATE TABLE Employee_inventary ( code_employee VARCHAR(10) NOT NULL, code_inventary int NOT NULL, PRIMARY KEY(code_employee, code_inventary), FOREIGN KEY(code_employee) REFERENCES Employee (code), FOREIGN KEY(code_inventary) REFERENCES Inventary (code) )";
+		String createEmployeeInventaryTable = "CREATE TABLE Employee_inventary ( code_employee VARCHAR(10) NOT NULL, code_inventary int NOT NULL, PRIMARY KEY(code_employee, code_inventary), FOREIGN KEY(code_employee) REFERENCES Employee (code), FOREIGN KEY(code_inventary) REFERENCES Inventary (code), FOREIGN KEY(code_payment_type) REFERENCES Payment_type (code) )";
 		String createProductInventaryTable = "CREATE TABLE Product_inventari ( code_product int NOT NULL, code_inventary int NOT NULL, quantity int NOT NULL, FOREIGN KEY(code_product) REFERENCES Product (code), FOREIGN KEY(code_inventary) REFERENCES Inventary (code) )";
-		String createProductSaleTable = "CREATE TABLE Product_sale ( code_product int NOT NULL, code_sale int NOT NULL, FOREIGN KEY(code_product) REFERENCES Product (code), FOREIGN KEY(code_sale) REFERENCES Sale (code) )";
+		String createProductSaleTable = "CREATE TABLE Product_sale ( code_product int NOT NULL, code_sale int NOT NULL, quantity int NOT NULL, FOREIGN KEY(code_product) REFERENCES Product (code), FOREIGN KEY(code_sale) REFERENCES Sale (code) )";
 		String createClientServiceTable = "CREATE TABLE Client_service ( code_client int NOT NULL, code_service int NOT NULL, FOREIGN KEY(code_client) REFERENCES Client (code), FOREIGN KEY(code_service) REFERENCES Service (code) )";
-
 		String createProductProviderTable = "CREATE TABLE Product_provider ( code_product int NOT NULL, code_provider int NOT NULL, PRIMARY KEY(code_product, code_provider), FOREIGN KEY(code_product) REFERENCES Product (code), FOREIGN KEY(code_provider) REFERENCES Provider (code) )";
 
 		createTable(createClientTable);
@@ -132,19 +130,24 @@ public class InitDatabase {
 		createTable(createProductProviderTable);
 
 		/**
+		 * + La relación entre venta y pago es de 1 a N (un medio de pago puede estar en
+		 * varias ventas) ?????
+		 * 
+		 * 
+		 * + Revisar las relaciones entre cliente, cliente-servicio, servicio y
+		 * factura-servicio. El patron de este tipo de tablas sería: Un cliente tiene
+		 * varias Factura-Servicio, una factura servicio tiene detalle servicio y un
+		 * servicio puede estar en varios detalles de servicio. ????
+		 * 
+		 * 
 		 * + cargo_actual es un grupo repetido + A empleado-Inventario y
 		 * producto-Inventario le hace falta la llave primaria + Es posible que un
 		 * producto sea suminitrado por varios proveedores, de esta forma faltaría una
 		 * tabla intermedia + En producto-venta falta el atributo cantidad + La relación
-		 * entre venta y pago es de 1 a N (un medio de pago puede estar en varias
-		 * ventas) + Revisar las relaciones entre cliente, cliente-servicio, servicio y
-		 * factura-servicio. El patron de este tipo de tablas sería: Un cliente tiene
-		 * varias Factura-Servicio, una factura servicio tiene detalle servicio y un
-		 * servicio puede estar en varios detalles de servicio. + La relación contrato y
-		 * tipoContrato es al contrario es decir un tipo de contrato puede estar en
-		 * varios contratos. Lo mismo que cargo, en un cargo pueden haber varios
-		 * empleados + La tabla Domicilio es como la ruta que sea crea (supongo), no
-		 * entiendo porque tiene el atributo dirección y teléfono?
+		 * contrato y tipoContrato es al contrario es decir un tipo de contrato puede
+		 * estar en varios contratos. Lo mismo que cargo, en un cargo pueden haber
+		 * varios empleados + La tabla Domicilio es como la ruta que sea crea (supongo),
+		 * no entiendo porque tiene el atributo dirección y teléfono?
 		 */
 	}
 }
